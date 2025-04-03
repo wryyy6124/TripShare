48cf2ec3a47dbfd6865512183c55f75af4dfa6e5
k.tamura <wryyy.6124@gmail.com>
2025-04-03 18:05:58 +0900

    2025/04/03_お問い合わせセクション_スクリプト実装

--- js/script.js
+++ js/script.js
@@ -8,26 +8,50 @@ const $doc = document;
 // ------------------------------
 $doc.addEventListener("DOMContentLoaded", () => {
   // ------------------------------
-  // ヘッダーのDOMを取得
+  // 画像のDOMを取得～フェードアニメーション付与
+  // ------------------------------
+  const $images = document.querySelectorAll("#Main img");
+
+  const imagesObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("is_visible");
+          imagesObserver.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  $images.forEach((image) => imagesObserver.observe(image));
+
+  // ------------------------------
+  // ヘッダー関連のスクリプト
   // ------------------------------
   const $header = $doc.querySelector("#Header");
+  const $catchCopy = document.querySelector(".hero__block");
 
-  window.addEventListener("scroll", () => {
-    const headerHeight = $header ? $header.offsetHeight : 0;
+  if (!$header || !$catchCopy) return;
 
-    const heroScrollTop =
-      $doc.querySelector("#Hero").getBoundingClientRect().bottom - headerHeight;
-
-    if (heroScrollTop <= 0) {
-      $header.classList.add("is_scrolled");
-    } else {
-      $header.classList.remove("is_scrolled");
+  // スクロール位置がヒーローイメージの座標を越えると発動
+  const headerObserver = new IntersectionObserver(
+    ([entry]) => {
+      if (!entry.isIntersecting) {
+        $header.classList.add("is_scrolled");
+      } else {
+        $header.classList.remove("is_scrolled");
+      }
+    },
+    {
+      root: null,
+      threshold: 0,
+      rootMargin: `-${$catchCopy.offsetHeight}px 0px 0px 0px`,
     }
-  });
+  );
+  headerObserver.observe($catchCopy);
 
-  // ------------------------------
-  // ハンバーガーメニュー
-  // ------------------------------
+  // ハンバーガーメニュー　※SPレイアウト時のみ
   const $hamburger = $doc.querySelector(".header__hamburger");
 
   $hamburger.addEventListener("click", () => {
@@ -42,7 +66,7 @@ $doc.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("resize", resizeWindow);
 
   // ------------------------------
-  // アンカーリンク
+  // スムーススクロール
   // ------------------------------
   const $anchorLink = $doc.querySelectorAll('a[href^="#"]');
 
@@ -64,5 +88,82 @@ $doc.addEventListener("DOMContentLoaded", () => {
         });
       }
     });
+  });
+
+  // ------------------------------
+  // フォーム関連
+  // ------------------------------
+  const $form = $doc.querySelector(".contact__form");
+  const radioBody = $doc.querySelector(".contact__form__radio");
+  const radioButtons = $doc.querySelectorAll('input[name="contact"]');
+  const emailInput = $doc.querySelector("#Email");
+
+  // エラーメッセージを表示させる。
+  const showErrorBox = (value, message) => {
+    let textBox = value.nextElementSibling;
+
+    if (!textBox || !textBox.classList.contains("errorMessage")) {
+      textBox = $doc.createElement("span");
+      textBox.classList.add("errorMessage");
+
+      value.after(textBox);
+    }
+
+    textBox.textContent = message;
+  };
+
+  // エラーメッセージを削除する。
+  const clearErrorBox = (value) => {
+    const textBox = value.nextElementSibling;
+
+    if (textBox && textBox.classList.contains("errorMessage")) {
+      textBox.remove();
+    }
+  };
+
+  // フォーム送信時のバリデーション
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let isValid = true;
+
+    // 「種別」のバリデーション
+    // HTMLソースではデフォルトで１点目に選択させているが、
+    // もしチェックが外れていた場合の為に念のため判定する。
+    const isRadioChecked = Array.from(radioButtons).some(
+      (radio) => radio.checked
+    );
+
+    if (!isRadioChecked) {
+      showErrorBox(radioBody, "※種別を選択してください。");
+      isValid = false;
+    } else {
+      clearErrorBox(radioBody);
+    }
+
+    // 「メールアドレス」のバリデーション
+    // 空欄チェック & メールアドレスの体裁となっているか判定する。
+    if (!emailInput.value.trim()) {
+      showErrorBox(emailInput, "※メールアドレスを入力してください。");
+      isValid = false;
+    } else if (!emailInput.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+      showErrorBox(emailInput, "※有効なメールアドレスを入力してください。");
+      isValid = false;
+    } else {
+      clearErrorBox(emailInput);
+    }
+
+    if (isValid) {
+      alert(
+        "お問い合わせ内容を送信しました。\n内容を確認後、入力頂いたメールアドレスへ返答しますのでお待ちください。"
+      );
+
+      emailInput.value = "";
+      $doc.querySelector("#Inquiry").value = "";
+
+      return;
+    }
+
+    alert("必須項目が選択・入力されていません。");
   });
 });
